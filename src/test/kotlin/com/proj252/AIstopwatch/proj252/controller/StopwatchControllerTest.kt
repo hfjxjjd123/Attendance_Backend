4a089f06977f18cf66b15804e2efb7e2878d18f5
hfjxjjd123 <hakrim@ihaglim-ui-MacBookAir.local>
2023-02-09 10:04:43 +0900

    서버 실행처리

---
+++ src/test/kotlin/com/proj252/AIstopwatch/proj252/controller/StopwatchControllerTest.kt
@@ -0,0 +1,24 @@
+package com.proj252.AIstopwatch.proj252.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class StopwatchControllerTest {
+
+    @Test
+    fun runStopwatch() {
+    }
+
+    @Test
+    fun pauseStopwatch() {
+    }
+
+    @Test
+    fun syncStopwatch() {
+    }
+
+    @Test
+    fun getTotalTime() {
+    }
+}
