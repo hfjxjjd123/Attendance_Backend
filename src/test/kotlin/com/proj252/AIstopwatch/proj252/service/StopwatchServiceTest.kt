778f54fbc89c80b1159666cbf643d1482708affd
hfjxjjd123 <hakrim@ihaglim-ui-MacBookAir.local>
2023-02-15 14:10:42 +0900

    Mockito로 Repo 코드 검사 완료

--- src/test/kotlin/com/proj252/AIstopwatch/proj252/service/StopwatchServiceTest.kt
+++ src/test/kotlin/com/proj252/AIstopwatch/proj252/service/StopwatchServiceTest.kt
@@ -1,22 +1,13 @@
 package com.proj252.AIstopwatch.proj252.service
 
-import com.proj252.AIstopwatch.proj252.repository.SdjTmpReportRepo
 import org.junit.jupiter.api.Test
 
-import org.mockito.Mock
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.junit.jupiter.api.Assertions.*
 
-@DataJpaTest
-//pass
 class StopwatchServiceTest {
 
-    @Mock
-    lateinit var stopwatchRepo: SdjTmpReportRepo
-
-    lateinit var stopwatchService: StopwatchService
-
     @Test
-    fun getStopwatch() {
+    fun getTotalTime() {
     }
 
     @Test
