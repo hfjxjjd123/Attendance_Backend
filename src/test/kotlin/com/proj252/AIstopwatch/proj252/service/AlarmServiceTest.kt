38929a88989b3f67430e318cbdc7fe98e8c7a4de
hfjxjjd123 <hakrim@ihaglim-ui-MacBookAir.local>
2023-02-15 14:48:41 +0900

    Alarm Repo 코드 테스트완료

---
+++ src/test/kotlin/com/proj252/AIstopwatch/proj252/service/AlarmServiceTest.kt
@@ -0,0 +1,16 @@
+package com.proj252.AIstopwatch.proj252.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class AlarmServiceTest {
+
+    @Test
+    fun setAlarm() {
+    }
+
+    @Test
+    fun getAlarm() {
+    }
+}
