fe9599583f76ee1562e6c7d7bdda458f334dcbe9
hfjxjjd123 <licbusiness21@gmail.com>
2023-02-16 11:35:29 +0900

    User->Auth로 이름 수정

--- src/main/kotlin/com/proj252/AIstopwatch/proj252/repository/SdjUserRepo.kt
+++ src/main/kotlin/com/proj252/AIstopwatch/proj252/repository/SdjUserRepo.kt
@@ -2,8 +2,10 @@ package com.proj252.AIstopwatch.proj252.repository
 
 import com.proj252.AIstopwatch.proj252.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface SdjUserRepo: JpaRepository<User,Long> {
     @Override
     override fun findById(id: Long): Optional<User>
