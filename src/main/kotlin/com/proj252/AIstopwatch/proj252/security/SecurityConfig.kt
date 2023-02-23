08219ab548226c446e6074266900ee4754806c72
hfjxjjd123 <licbusiness21@gmail.com>
2023-02-23 14:21:02 +0900

    GoogleOAuth2UserService 복붙완성, 커스터마이징 필요

--- src/main/kotlin/com/proj252/AIstopwatch/proj252/security/SecurityConfig.kt
+++ src/main/kotlin/com/proj252/AIstopwatch/proj252/security/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.proj252.AIstopwatch.proj252.security
 
+import com.proj252.AIstopwatch.proj252.service.GoogleOAuthUserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.stereotype.Service
 
 
 @Configuration
@@ -25,34 +27,13 @@ class SecurityConfig {
             .anyRequest().authenticated()
             .and()
             .oauth2Login()
-            .loginPage("/login") //로그인이 이뤄질 페이지 명시
+            .loginPage("/login") //!!로그인이 이뤄질 페이지 명시
             .userInfoEndpoint()
-            .userService(oauth2UserService())
+            .userService(GoogleOAuthUserService())
             .and()
             .successHandler(oauth2AuthenticationSuccessHandler())
 
 
         return http.build()
     }
-    @Bean
-    fun oauth2UserService(): OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-        val delegate = DefaultOAuth2UserService()
-        return OAuth2UserService { userRequest ->
-            // Retrieve user from OAuth2 provider
-            val oAuth2User = delegate.loadUser(userRequest)
-            val authorities = oAuth2User.authorities.map { it.authority }
-            // Create our own user object with OAuth2 user details
-            CustomUserDetails(oAuth2User.name, authorities)
-        }
-    }
-
-    @Bean
-    fun oauth2AuthenticationSuccessHandler(): AuthenticationSuccessHandler {
-        return SimpleUrlAuthenticationSuccessHandler("/home")
-    }
-
-
-//    override fun configure(auth: AuthenticationManagerBuilder) {
-//        // configure your user store here (e.g., in-memory, JDBC, LDAP)
-//    }
 }
