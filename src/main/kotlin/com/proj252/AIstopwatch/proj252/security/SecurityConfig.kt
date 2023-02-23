200a10d92e1862f69b7dbf017e14ef5389745a04
hfjxjjd123 <licbusiness21@gmail.com>
2023-02-23 15:44:09 +0900

    Securities..

--- src/main/kotlin/com/proj252/AIstopwatch/proj252/security/SecurityConfig.kt
+++ src/main/kotlin/com/proj252/AIstopwatch/proj252/security/SecurityConfig.kt
@@ -1,21 +1,11 @@
 package com.proj252.AIstopwatch.proj252.security
 
 import com.proj252.AIstopwatch.proj252.service.GoogleOAuth2UserService
-import com.proj252.AIstopwatch.proj252.service.GoogleOAuthUserService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
-import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
-import org.springframework.stereotype.Service
 
 
 @Configuration
@@ -32,7 +22,7 @@ class SecurityConfig {
             .userInfoEndpoint()
             .userService(GoogleOAuth2UserService())
             .and()
-            .successHandler(oauth2AuthenticationSuccessHandler())
+            .successHandler(OAuth2LoginSuccessHandler())
 
 
         return http.build()
